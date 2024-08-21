316159671b3e90c193b353befca7afaa414c3413
ADSLustosa <adslustosa@gmail.com>
2024-08-21 12:48:47 -0300

    Atualizado

---
+++ Java/Formação Java/Teste/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+
+    public static void main(String[] args){
+        int numero1 = 2;
+        int numero2 = 2;
+        int soma = numero1 + numero2;
+        System.out.println(soma);
+    }
+}
