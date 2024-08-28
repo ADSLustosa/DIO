e54eeb04a96d99ee033875fe1f0862dc0cbecdb3
ADSLustosa <adslustosa@gmail.com>
2024-08-28 18:42:21 -0300

    Atualizado

--- Java/Formação Java/Teste/src/MinhaClasse.java
+++ Java/Formação Java/Teste/src/MinhaClasse.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class MinhaClasse {
 
     public static void main(String[] args){
-        int numero1 = 2;
-        int numero2 = 2;
-        int soma = numero1 + numero2;
-        System.out.println(soma);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro número: ");
+            int numero1 = scanner.nextInt();
+            System.out.println("Digite o segundo número: ");
+            int numero2 = scanner.nextInt();
+            int soma = soma(numero1, numero2);
+            System.out.println("A soma de "  + numero1 + " + " + numero2 + " é: " + soma);
+        }
     }
+
+    public static int soma(int numero1, int numero2){
+        return numero1+numero2;
+    }
+
 }
